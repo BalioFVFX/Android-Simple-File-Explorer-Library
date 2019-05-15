f898b0b5f1a575112be4343f0670700d50c46d94
BalioFVFX <vfxbaliof@gmail.com>
2019-05-15 11:34:03 +0300

    Removed the empty fragment when the back button is pressed. (BUGFIX)

--- simplefileexplorer/src/main/java/com/example/simplefileexplorer/SimpleFileExplorerActivity.java
+++ simplefileexplorer/src/main/java/com/example/simplefileexplorer/SimpleFileExplorerActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Environment;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +13,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.List;
+
 
 public class SimpleFileExplorerActivity extends AppCompatActivity implements ActivityListener{
 
